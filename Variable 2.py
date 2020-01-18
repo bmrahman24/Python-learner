5b50ee8272671febd055536576cfb025b0f8c5fc
bmrahman24 <59472850+bmrahman24@users.noreply.github.com>
2020-01-19 01:28:34 +0600

    More Variable
    More Variable

---
+++ Variable 2.py
@@ -0,0 +1,18 @@
+#no_of_car = 2
+#my_name = """Mustakur Rahman's" "Programming" made simple"""
+#no_of_car = no_of_car + 10
+#print("My name is", my_name,"and My number of car are", no_of_car)
+#print(f"My name is {my_name} and my number of cars are {no_of_car}")
+
+my_name = """Mustakur\'s Food list
+\t->pizaa
+\t->Pasta
+\t->Kacchi
+"""
+print(my_name)
+print(my_name,end="\t")
+
+
+
+
+
