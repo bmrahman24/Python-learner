e61a6f6e4a1d26110cf2957bc1ab36a10c63fa44
bmrahman24 <59472850+bmrahman24@users.noreply.github.com>
2020-01-22 19:34:47 +0600

    Basic Operation
    Basic Operation and python beginne

---
+++ Basic oparation.py
@@ -0,0 +1,17 @@
+"""
+ print(2+6)
+print(5+4-3)
+
+print(2*(2+2))
+
+print(20/2)
+
+print(-7 + 2)
+
+"""
+print(6*7.0)
+
+print(4+1.65)
+
+print(-7+2)
+
---
+++ pythonbeginner.py
@@ -0,0 +1,304 @@
+"""
+variable amd math
+
+ver1 = 5
+ver2 = 15
+ver3 = ver1 + ver2
+print(ver3)
+
+print("Hello i\'m hasan")
+print("Hello i'm hasan")
+"""
+
+#myVar = "I love Bangladesh"
+#print(myVar)
+
+# + - * /
+
+"""
+myVar = "hello i am "
+name= "Hasan"
+print(myVar+name)
+
+text = " Two and Two Makes "
+math = 2 + 2
+
+result = str(math)
+
+print(text + result)
+
+x,y,z = (3,4,5)
+print(x)
+print(y)
+print(z)
+
+#if else statement
+
+age = 20
+
+if(age >= 18):
+    print("You are an adult")
+    print("hello")
+    
+else:
+    print("You are not an adult")
+
+# if else statement
+
+age = 15
+
+if(age <= 13):
+    print("You are a kid")
+
+elif(age <= 18):
+    print("You are a teen ager")
+    
+else:
+    print("You are an adult")
+    
+    
+a = 10
+a = 4
+a = 80
+print(a)
+
+"""
+#while loop
+
+#while condition:
+    #code
+    #condition
+"""
+i = 1
+while i<=10:
+    print("I love Python")
+    i += 1
+    
+i = 1
+while i<=100:
+    if i%2==0:
+        print(i)
+        
+    i +=1    
+
+i = 1
+while i <= 100:
+    if i % 2 == 0:
+        print(i)
+
+    i += 1
+    
+#even/odd
+
+i = 1
+
+if i%2 == 0:
+    print("Even")
+    
+else:
+    print("Odd")
+# For Loop
+data = [5,6,7,90,23,67,69,70]
+i = 0
+
+while i<= 8:
+    print(data[i])
+    i+=1
+ 
+# For Loop
+
+data = [5,6,7,90,23,67,69,70]
+
+for x in data:
+    print(x)
+        
+# Functions
+
+def simpleFunction():
+    print("say hello to function")
+    a,b = 5,10
+    sum = a+b
+    print(sum)
+simpleFunction()
+simpleFunction()
+
+def add(n1, n2):
+   return n1 + n2
+
+n = 10
+m = 5
+result = add(n, m)
+print(result)
+
+
+# Function Parameters
+
+def sum(num1, num2,num3):
+    sum = num1 + num2 + num3
+    print(sum)
+
+sum(5,4,9)
+
+"""
+var = 10
+
+def function():
+	loc = var
+	loc = loc + 1
+	print(var)
+
+function()	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
